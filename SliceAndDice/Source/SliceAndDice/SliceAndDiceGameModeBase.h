e21865a92975b56acdd5a7233b453681fe5b6f24
Julaxe <julaxe8@gmail.com>
2022-09-27 16:29:45 -0400

    Unreal 5 project blank created

---
+++ SliceAndDice/Source/SliceAndDice/SliceAndDiceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SliceAndDiceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLICEANDDICE_API ASliceAndDiceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
